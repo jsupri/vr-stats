cf1db439a9a5227dcfff8ffbdac93c16bc30ea4f
jsupri <63751163+jsupri@users.noreply.github.com>
2020-04-16 13:18:33 -0500

    Add files via upload

---
+++ movefiles.sh
@@ -0,0 +1,10 @@
+cp /root/vr-stats-master/Export-VM-List.ps1 /root
+cp /root/vr-stats-master/importdata.sql /root
+cp /root/vr-stats-master/createtables.sql /root
+cp /root/vr-stats-master/getTable.php /var/www/html/
+cp /root/vr-stats-master/index.php /var/www/html/
+cp /root/vr-stats-master/selector.php /var/www/html/
+cp /root/vr-stats-master/dbCreds.php /var/www/html/common
+cp /root/vr-stats-master/head.php /var/www/html/common
+cp /root/vr-stats-master/navBar.php /var/www/html/common
+cp /root/vr-stats-master/statsMainCSS.css /var/www/html/common
